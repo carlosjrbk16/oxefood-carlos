859105364d99b011f084f8aeaa6380d384ca86a5
carlojrbk16 <carlosjrbk@16.com>
2022-10-06 21:16:13 -0300

    primeiro commit

---
+++ src/main/java/br/com/ifpe/oxefoodcarlos/OxefoodCarlosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodcarlos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodCarlosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodCarlosApplication.class, args);
+	}
+
+}
